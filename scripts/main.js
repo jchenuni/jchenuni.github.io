3b61f2fea90584cd8219a28cdb982f1aa6958a16
Jian <jchen93@illinois.edu>
2023-07-21 14:25:31 +0900

    5th

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,4 @@
-// initialise fullpage.js
-new fullpage('#fullpage', {
-  licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
-  anchors: ['charts'],
-  sectionsColor: ['#f7f3de'],
-  autoScrolling: true,
-  fitToSection: true,
-  slidesNavigation: true,
-  slidesNavPosition: 'top',
 
-  scrollOverflow: true,
-
-  // events
-  afterSlideLoad: function(section, origin, destination, direction) {
-    var numberOfIntroduction = 2;
-    if (destination.index <= numberOfIntroduction || destination.index > numberOfIntroduction + 3) {
-      return;
-    }
-
-    var chartFunc = charts['chart' + (destination.index - numberOfIntroduction)];
-    chartFunc();
-  },
-  onSlideLeave: function(section, origin, destination, direction) {
-    if (destination.index === 6) {
-
-    }
-  }
-});
-
-// fullpage_api.setAllowScrolling(false);
 
 
 // use local object to cache json response
